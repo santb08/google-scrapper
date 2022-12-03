34bf286f373dc8455659f74c60ac7245d239bfcc
Santiago <scb9908@gmail.com>
2022-12-03 02:01:52 -0500

    feat: cli configured

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from playwright.sync_api import sync_playwright
-import requests
-import io
-from PIL import Image
 from concurrent.futures import ThreadPoolExecutor
+from PIL import Image
+from playwright.sync_api import sync_playwright
+import click
+import base64
+import io
+import os
+import requests
+import re
 
 
 INPUT_SELECTOR = 'input[name="q"]'
@@ -14,20 +18,39 @@ LOADING_STATE = 'div[data-status="1"]'
 END_OF_RESULTS_STATE = 'div[data-status="3"]'
 result = []
 
-def write_images(images):
-  file = open('./results.txt', 'w')
-  for image in images:
-    file.write(image + '\n')
 
-def img_down(link):
-  response  = requests.get(link).content
-  image_file = io.BytesIO(response)
-  image  = Image.open(image_file)
-  with open(link.split('/')[-1] , "wb") as f:
-    image.save(f , "JPEG")
-    # print("Success!!!!")
+def img_down(path, image_data):
+  try:
+    index = image_data['index']
+    image_name = '-'.join(image_data['alt'].split(' ')[:5])
+    image_name = f'{index}-{image_name}.jpeg'
+    url = image_data['url']
+    image = None
 
-def scrap_ijjmages():
+    if image_data['base64']:
+      print('Base 64')
+      # image = base64.urlsafe_b64decode(url)
+      # image = Image.open(image)
+      url = re.sub('^data:image/.+;base64,', '', url)
+      print(url)
+      image= Image.open(io.BytesIO(base64.urlsafe_b64decode(url)))
+
+    else:
+      # return
+      response  = requests.get(url).content
+      image_file = io.BytesIO(response)
+      image  = Image.open(image_file)
+
+
+    print('[Creating]', image_name)
+    with open(f'{path}/{image_name}', "wb") as f:
+      image.save(f , "JPEG")
+      # print("Success!!!!")
+  except Exception as e:
+    print(e)
+
+
+def scrap_images(query, limit):
   with sync_playwright() as pw:
     # create browser instance
     # we can choose either a Headful (With GUI) or Headless mode:
@@ -39,12 +62,12 @@ def scrap_ijjmages():
 
     # Input search
     input = page.locator(INPUT_SELECTOR)
-    input.type('Sea', delay=0)
+    input.type(query, delay=0)
     input.press('Enter')
 
     # Get all results from Google
-    while not page.locator(END_OF_RESULTS_STATE).is_visible():
-      page.mouse.wheel(0, 400)
+    while not page.locator(END_OF_RESULTS_STATE).is_visible() and len(page.query_selector_all('.rg_i')) < limit:
+      page.mouse.wheel(0, 600)
 
       load_more_results = page.locator('.r0zKGf')
       if load_more_results.is_visible():
@@ -54,18 +77,55 @@ def scrap_ijjmages():
       if show_more_results.is_visible():
         show_more_results.click()
 
-    images = page.query_selector_all('.rg_i')
+    images = page.query_selector_all('.rg_i')[0:limit]
 
-    for img in images:
+    for index in range(len(images)):
+      img = images[index]
       image_url = img.get_attribute('src') or img.get_attribute('data-src')
-      result.append(image_url)
+
+      image_data = {
+        'alt': img.get_attribute('alt'),
+        'url': image_url,
+        'index': index,
+        'base64': 'base64' in image_url[0:30]
+      }
+
+      result.append(image_data)
 
     return result
 
-write_images(result)
+
+@click.command()
+@click.option(
+  '--output',
+  help='Output folder.',
+  required=True,
+)
+@click.option(
+  '--query',
+  prompt='Query to scrap from Google Images',
+  help='The person to greet.',
+)
+@click.option(
+  '--limit',
+  help="The limit of images",
+  default=50,
+)
+def main(output, query, limit):
+  all_images = scrap_images(query, limit)
+
+  if not os.path.exists(output):
+   os.makedirs(output)
+   print("Output folder not existing, created")
+
+  path = os.path.abspath(output)
+  print('[Downloading At]', path)
+
+  with ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(lambda x: img_down(path, x) , all_images)
+
 
 if __name__ == '__main__':
-  all_images = scrap_images()
-  with ThreadPoolExecutor(max_workers=10) as executor:
-      executor.map(img_down , all_images)
-  print("All Images Downloaded Successfully")
+  main()
+
+print("All Images Downloaded Successfully")
